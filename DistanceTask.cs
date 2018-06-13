e90904458f3e388506b06cc4a046674a6beabba7
Ira74 <minibenni74@gmail.com>
2018-06-13 21:11:50 +0500

    Решение задачи расстояние до отрезка

---
+++ DistanceTask.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace DistanceTask
+{
+    public static class DistanceTask
+
+    {
+
+        //Задание
+        //Напишите метод вычисления расстояния от отрезка до точки.
+        //Решение(выполнено самостоятельно):
+        public static double GetDistanceToSegment(double ax, double ay, double bx, double by,
+          double x, double y)
+        {
+
+            double distanceToLine = ((by - ay) * x) - ((bx - ax) * y) + (bx * ay) - (by * ax);
+            if (distanceToLine < 0)
+            {
+                distanceToLine = -1 * distanceToLine;
+            }
+
+            double distanceAB = Math.Sqrt((ax - bx) * (ax - bx) + (ay - by) * (ay - by));
+            double scalarMultiplicationAXAB = (x - ax) * (bx - ax) + (y - ay) * (by - ay);
+            double scalarMultiplicationBXAB = (x - bx) * (-bx + ax) + (y - by) * (-by + ay);
+            if (distanceAB == 0) return DistanceToPoint(y, ay, x, ax);
+            else if (scalarMultiplicationAXAB >= 0 && scalarMultiplicationBXAB >= 0)
+            {
+                double distanceGyotenuse;
+                return distanceGyotenuse = distanceToLine / (Math.Sqrt(Math.Pow(by - ay, 2) + Math.Pow(bx - ax, 2)));
+
+            }
+            else return Math.Min(DistanceToPoint(y, ay, x, ax), DistanceToPoint(y, by, x, bx));
+
+        }
+
+        public static double DistanceToPoint(double yToPoint, double byToPoint, double xToPoint, double bxToPoint)
+        {
+            return Math.Sqrt(Math.Pow((yToPoint - byToPoint), 2) + Math.Pow((xToPoint - bxToPoint), 2));
+
+        }
+
+    }
+
+}
+
+
